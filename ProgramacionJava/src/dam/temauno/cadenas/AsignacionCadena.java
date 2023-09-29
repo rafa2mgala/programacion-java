8b36e44cc5c09b4ca413dbe84ca342d7e6bba16a
rafa <rafa@raul-laptop.home>
2023-09-29 07:38:42 +0200

    Ejemplos sobre asignación a una variable de tipo String

---
+++ ProgramacionJava/src/dam/temauno/cadenas/AsignacionCadena.java
@@ -0,0 +1,19 @@
+package dam.temauno.cadenas;
+/**
+ * Clase de ejemplo para asignación de cadenas
+ * @author rafa
+ *
+ */
+public class AsignacionCadena {
+
+	public static void main(String[] args) {		
+		//inicialización como tipo de datos primitivo
+		String cadenaUno = "Cadena inicializada como tipo primitivo";
+		//inicialización como tipo de dato objeto
+		String cadenaDos = new String("Cadena inicializada como tipo objeto");
+		//se muestra el valor de las variables por consola
+		System.out.println(cadenaUno);
+		System.out.println(cadenaDos);
+	}
+
+}
