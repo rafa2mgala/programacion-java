bc88f891e2e266ff8d14220abf73acf38292a8e6
rafa <rafa@pc54-9.14005298.14.andared.ced.junta-andalucia.es>
2023-10-18 11:43:34 +0200

    Ejemplo de encapsulación sobre objetos de tipo Transporte

---
+++ ProgramacionJava/src/dam/temados/poo/encapsulacion/TestTransporte.java
@@ -0,0 +1,11 @@
+package dam.temados.poo.encapsulacion;
+
+public class TestTransporte {
+
+	public static void main(String[] args) {
+		//inicializar una variable de tipo Transporte
+		Transporte transporte = new Transporte();
+		transporte.setMarca("aaaaa"); //no hay encapsulación
+	}
+
+}
---
+++ ProgramacionJava/src/dam/temados/poo/encapsulacion/Transporte.java
@@ -0,0 +1,12 @@
+package dam.temados.poo.encapsulacion;
+
+public class Transporte {
+	private int velocidad;
+	private String marca;
+	private String modelo;
+	private long kilometraje;
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+}
