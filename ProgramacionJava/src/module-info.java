860cdc9df1b8c5ed7726a23dda5b6d93b22cd044
rafa <rafa@pc-profesor>
2023-11-23 19:39:32 +0100

    Configuración del módulo del proyecto

--- ProgramacionJava/src/module-info.java
+++ ProgramacionJava/src/module-info.java
@@ -1,8 +1,10 @@
 /**
  * 
  */
-/**
- * 
- */
 module ProgramacionJava {
+	requires java.base;
+	requires java.desktop;
+	//requires log4j.api;
+	requires java.logging;
+	opens dam.tematres.logger;
 }
